655ed0e0f37c9b486e50cd78d177d412ae9cb78f
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-08 09:14:40 -0500

    Solve the eighth exercise

---
+++ BasicOperations/expressions.py
@@ -0,0 +1,5 @@
+
+# To what value does the following expression evaluate?
+
+print('12' < '9')
+# The result will be True because the string '12' is less than the string '9' when compared lexicographically.

9a2411196a209d7c015cfd3fd9796691ac275273
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-08 08:46:48 -0500

    Solve the fourth exercise converting string to numbers

--- BasicOperations/concatenation.py
+++ BasicOperations/concatenation.py
@@ -6,3 +6,6 @@ print(fullName)
 
 print('5' + '10')
 # The result of this operation is 510, because the numbers are considered as strings
+
+print(int('5' + '10'))
+# The result of this operation is 15 because the numbers are converted to integers

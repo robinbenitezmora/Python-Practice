20907984879d92f5f3c58bc8f319ed43d387dfc7
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-08 08:40:05 -0500

    Solve the fourth exercise

--- BasicOperations/concatenation.py
+++ BasicOperations/concatenation.py
@@ -3,3 +3,6 @@ name = 'Robin'
 lastName = 'Benitez'
 fullName = name + ' ' + lastName
 print(fullName)
+
+print('5' + '10')
+# The result of this operation is 510, because the numbers are considered as strings

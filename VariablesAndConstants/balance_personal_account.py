b102996a069533b102c5e5bc95a5a6cce5ae7c82
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-09 13:38:47 -0500

    Solve the nineth exercise

--- VariablesAndConstants/balance_personal_account.py
+++ VariablesAndConstants/balance_personal_account.py
@@ -1,8 +1,8 @@
 
 amount = int(input('Write the amount to deposit: '))
 interest = float(input('Please define the interest to pay: '))
-months = int(input('How many months would you like to calculate your money?: '))
+years = int(input('How many years would you like to calculate your money?: '))
 
-amount *= round((1 + (interest / 100))**(months), 5)
+amount *= round((1 + (interest / 100))**(years), 8)
 print(amount)
 

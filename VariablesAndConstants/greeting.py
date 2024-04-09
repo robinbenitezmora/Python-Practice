3914ea8c399da7cf5192e95e9e30c614080ae762
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-09 07:45:40 -0500

    Define another option

--- VariablesAndConstants/greeting.py
+++ VariablesAndConstants/greeting.py
@@ -5,3 +5,6 @@ print('Good Morning ' + name)
 print('Good Afternoon ' + name)
 print('Good Evening ' + name)
 
+print(f'Good Morning, {name}')
+print(f'Good Afternoon, {name}')
+print(f'Good Evening, {name}')

f1bb9340bc68a8822230c5c2f34d5cb0bfc1de7d
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-09 07:49:47 -0500

    Create age.py file in order to define sixth excercise

---
+++ VariablesAndConstants/age.py
@@ -0,0 +1,8 @@
+
+age = 22
+
+print('You are ' + str(age) + ' years old')
+print('In 10 years, you will be ' + str(age + 10) + ' years old')
+print('In 20 years, you will be ' + str(age + 20) + ' years old') 
+print('In 30 years, you will be ' + str(age + 30) + ' years old')
+print('In 40 years, you will be ' + str(age + 40) + ' years old')

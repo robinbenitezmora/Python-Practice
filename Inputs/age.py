5c82940cb53c161bb29d864c59bb3741fefc76b8
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-10 09:31:46 -0500

    Create age.py file

---
+++ Inputs/age.py
@@ -0,0 +1,7 @@
+
+age = int(input("Enter your age: "))
+print(f'You are {age} years old.')
+print(f'In 10 years, you will be {age + 10} years old.')
+print(f'In 20 years, you will be {age + 20} years old.')
+print(f'In 30 years, you will be {age + 30} years old.')
+print(f'In 40 years, you will be {age + 40} years old.')

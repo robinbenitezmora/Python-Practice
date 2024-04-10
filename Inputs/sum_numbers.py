01bce6a7e5ccb6f99b42dc4b71bf9c6fcda04864
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-10 09:31:11 -0500

    Create sum_numeric.py file

---
+++ Inputs/sum_numbers.py
@@ -0,0 +1,6 @@
+
+number1 = input("Enter the first number: ")
+number2 = input("Enter the second number: ")
+sum = float(number1) + float(number2)
+
+print(f'The sum of {number1} and {number2} is {sum}')

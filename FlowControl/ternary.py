c466d5bc615ade70bcb2e68e4725aaa6bca9e23a
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-12 09:07:20 -0500

    Solve fourth exercise about Flow Control

---
+++ FlowControl/ternary.py
@@ -0,0 +1,11 @@
+
+option = input("Enter a color: ")
+
+def ternary(option):
+    if option == 'red':
+        return 'You entered red'
+    else:
+        return 'You did not enter red'
+
+print(ternary(option))
+

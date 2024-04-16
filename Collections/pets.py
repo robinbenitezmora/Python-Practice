4f79a84fe5967d4116314a973a3becd1a41a005a
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-16 13:40:39 -0500

    Solve the fourth Using Collection problem

---
+++ Collections/pets.py
@@ -0,0 +1,15 @@
+
+pets = {
+    'Cat': 'Meow',
+    'Dog': 'Bark',
+    'Bird': 'Tweet',
+}
+
+def get_pet_sound(pet):
+    for key, value in pets.items():
+        if key == pet:
+            return value
+    return f'silence'
+
+print(get_pet_sound('Dog'))  # Bark
+print(get_pet_sound('Lizard'))  # None

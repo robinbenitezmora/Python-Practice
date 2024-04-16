d05969867c48393cad6696c1bf0267099075b4f9
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-16 15:07:31 -0500

    Solve the eleventh Using Collection hash problem

---
+++ Collections/nested_2.py
@@ -0,0 +1,26 @@
+
+cats = {
+    'Pete': {
+        'Cheddar': {
+            'color': 'orange',
+            'enjoys': {
+                'sleeping',
+                'snuggling',
+                'meowing',
+                'eating',
+                'birdwatching',
+            },
+        },
+        'Cocoa': {
+            'color': 'black',
+            'enjoys': {
+                'eating',
+                'sleeping',
+                'playing',
+                'chewing',
+            },
+        },
+    },
+}
+
+print(cats['Pete']['Cocoa']['enjoys'])

c5ceb73e69f70d74fb8f0a6983a5e79c7705e3fc
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-18 07:51:35 -0500

    Modify nineth exercise about factorials

--- Loops/factorial_number.py
+++ Loops/factorial_number.py
@@ -1,8 +1,8 @@
 def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
 
 print(factorial(1))   # 1
 print(factorial(2))   # 2

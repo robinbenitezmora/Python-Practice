763413de3111fc8dd7f82844e720c5b91002b83c
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-18 07:48:58 -0500

    Solve nineth exercise about factorials

---
+++ Loops/factorial_number.py
@@ -0,0 +1,21 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(1))   # 1
+print(factorial(2))   # 2
+print(factorial(3))   # 6
+print(factorial(4))   # 24
+print(factorial(5))   # 120
+print(factorial(6))   # 720
+print(factorial(7))   # 5040
+print(factorial(8))   # 40320
+print(factorial(25))  # 15511210043330985984000000
+print(factorial(50))  # 30414093201713378043612608166064768844377641568960512000000000000
+
+
+
+
+

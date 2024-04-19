faee8adbc075b83177b5f8e37b6a201003f7d750
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-19 15:59:50 -0500

    Solve the 'sqrt' exercise

---
+++ MoreStuffs/sqrt.py
@@ -0,0 +1,9 @@
+
+from math import sqrt
+
+number = int(input("Enter a number: "))
+
+def square_root(number):
+    return sqrt(number)
+
+print(f"The square root of {number} is {square_root(number)}")

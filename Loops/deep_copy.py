069180c7a956408ca09e9f36da86d7fd185eccb3
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-19 14:07:01 -0500

    Modify the fifth exercise about Variables and Pointers

--- Loops/deep_copy.py
+++ Loops/deep_copy.py
@@ -1,18 +1,12 @@
 
-# You may modify this line
+import copy
 
 dict1 = {
     'a': [[7, 1], ['aa', 'aaa']],
     'b': ([3, 2], ['bb', 'bbb']),
 }
 
-dict2 = dict(dict1)
-dict2['a'] = list(dict1['a'])
-dict2['a'][0] = list(dict1['a'][0])
-dict2['a'][1] = list(dict1['a'][1])
-dict2['b'] = list(dict1['b'])
-dict2['b'][0] = list(dict1['b'][0])
-dict2['b'][1] = list(dict1['b'][1])
+dict2 = copy.deepcopy(dict1)
 
 # All of these should print False
 print(dict1         is dict2)

2b903a0bc3d98fbf8bc18673edd2af064741679d
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-19 14:33:12 -0500

    Move the fifth exercise to the new folder Variables and Pointers

---
+++ VariablesAndPointers/deep_copy.py
@@ -0,0 +1,18 @@
+
+import copy
+
+dict1 = {
+    'a': [[7, 1], ['aa', 'aaa']],
+    'b': ([3, 2], ['bb', 'bbb']),
+}
+
+dict2 = copy.deepcopy(dict1)
+
+# All of these should print False
+print(dict1         is dict2)
+print(dict1['a']    is dict2['a'])
+print(dict1['a'][0] is dict2['a'][0])
+print(dict1['a'][1] is dict2['a'][1])
+print(dict1['b']    is dict2['b'])
+print(dict1['b'][0] is dict2['b'][0])
+print(dict1['b'][1] is dict2['b'][1])

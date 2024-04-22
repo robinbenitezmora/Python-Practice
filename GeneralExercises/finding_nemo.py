fb992f13d849aa6d43c12ba6121587b6358e05f7
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-22 16:51:39 -0500

    Practice break structure by means finding_nemo.py

---
+++ GeneralExercises/finding_nemo.py
@@ -0,0 +1,8 @@
+
+fish_list = ['Dory', 'Marlin', 'Gill', 'Nemo', 'Bruce']
+
+for fish in fish_list:
+    if fish == 'Nemo':
+        print('I found Nemo!')
+        break
+    print('Where is Nemo?')

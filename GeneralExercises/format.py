43651c819f9bb927fb9a374608317e128907dcfd
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-22 16:49:29 -0500

    Practice format string structure by means format.py

---
+++ GeneralExercises/format.py
@@ -0,0 +1,8 @@
+
+name = "Victor"
+profession = "programmer"
+
+greeting1 = 'Hello, {}. You are a {}'.format(name, profession)
+greeting2 = f'Hello, {name}. You are a {profession}'
+print(greeting1)
+print(greeting2)

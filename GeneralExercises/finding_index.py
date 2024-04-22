8af0bae560b251d66dc0aa6b72d2bb566b530bb4
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-22 16:48:50 -0500

    Practice indexes by means finding_index.py

---
+++ GeneralExercises/finding_index.py
@@ -0,0 +1,8 @@
+
+fruits = ["apple", "banana", "cherry", "peach", "watermelon"]
+
+if 'cherry' in fruits:
+    index_cherry = fruits.index('cherry')
+    print(f'The word is in the list and its index is {index_cherry}')
+else:
+    print(f'The word is not in the list')

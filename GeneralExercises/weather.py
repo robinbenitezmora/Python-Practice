c484f76e7eb63ca74a3a6ff25c7dc4b6b3282338
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-23 07:13:27 -0500

    Practice the weather.py file in order to solve an if else exercise

---
+++ GeneralExercises/weather.py
@@ -0,0 +1,13 @@
+
+weather = input('What is the weather like today?\n')
+
+if weather == 'rainy':
+    print('Grab your umbrella!')
+elif weather == 'sunny':
+    print('It is a beautiful day!')
+elif weather == 'snowy':
+    print('Wear your boots!')
+elif weather == 'cloudy':
+    print('It might rain, take your umbrella!')
+else:
+    print('Let\'s stay inside!')

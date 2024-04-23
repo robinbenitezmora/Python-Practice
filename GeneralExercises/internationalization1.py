0eed7d0b8bb5912b1e1bee0d6a33af3db079b2ac
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-23 14:14:02 -0500

    Work on match-case structure in Python

---
+++ GeneralExercises/internationalization1.py
@@ -0,0 +1,23 @@
+greet = input('Enter your language in order to greet you:\n')
+
+match greet:
+    case 'en':
+        print('Hi!')
+    case 'fr':
+        print('Salut!')
+    case 'pt':
+        print('Olá!')
+    case 'de':
+        print('Hallo!')
+    case 'sv':
+        print('Hej!')
+    case 'es':
+        print('Hola!')
+    case 'it':
+        print('Ciao!')
+    case 'nl':
+        print('Hallo!')
+    case 'ru':
+        print('Privet!')
+    case _ :
+        print('Sorry, I do not speak that language.')

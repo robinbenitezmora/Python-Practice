85a8f365133b138b6f3618379098c67e6b964705
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-23 14:17:04 -0500

    Refactor the internationalization.py file

--- GeneralExercises/internationalization1.py
+++ GeneralExercises/internationalization1.py
@@ -1,23 +1,26 @@
 greet = input('Enter your language in order to greet you:\n')
 
-match greet:
-    case 'en':
-        print('Hi!')
-    case 'fr':
-        print('Salut!')
-    case 'pt':
-        print('Olá!')
-    case 'de':
-        print('Hallo!')
-    case 'sv':
-        print('Hej!')
-    case 'es':
-        print('Hola!')
-    case 'it':
-        print('Ciao!')
-    case 'nl':
-        print('Hallo!')
-    case 'ru':
-        print('Privet!')
-    case _ :
-        print('Sorry, I do not speak that language.')
+def greet_match(greet):
+    match greet:
+        case 'en':
+            print('Hi!')
+        case 'fr':
+            print('Salut!')
+        case 'pt':
+            print('Olá!')
+        case 'de':
+            print('Hallo!')
+        case 'sv':
+            print('Hej!')
+        case 'es':
+            print('Hola!')
+        case 'it':
+            print('Ciao!')
+        case 'nl':
+            print('Hallo!')
+        case 'ru':
+            print('Privet!')
+        case _ :
+            print('Sorry, I do not speak that language.')
+
+greet_match(greet)

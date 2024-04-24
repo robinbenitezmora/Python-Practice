310a3275cd18dada3eb95ac4f5d6a78cbeaf01a7
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-24 08:50:44 -0500

    Solve the problem about start_with substring exercise

---
+++ GeneralExercises/count_substring.py
@@ -0,0 +1,11 @@
+
+def count_substrings(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
+
+print(count_substrings("lemon lemon lemon", "lemon")) # 3
+print(count_substrings("laLAlaLA", "la")) # 2
+print(count_substrings("launch", "uno")) # 0

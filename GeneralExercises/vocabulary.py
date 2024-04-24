5b3580a8e441a83944e4f54e394ffbf1d0fd7bdd
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-24 10:05:35 -0500

    Print each of items in a nested list

---
+++ GeneralExercises/vocabulary.py
@@ -0,0 +1,13 @@
+
+vocabulary = [
+    ['happy', 'cheerful', 'merry', 'glad'],
+    ['tired', 'sleepy', 'fatigued', 'drained'],
+    ['excited', 'eager', 'enthused', 'animated'],
+]
+
+def print_vocabulary():
+    for list in vocabulary:
+        for word in list:
+            print(word)
+
+print_vocabulary()

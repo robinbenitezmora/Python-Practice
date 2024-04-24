3c2f982242e00d7ec328ebf41416fef4fe39a393
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-24 08:42:20 -0500

    Solve the problem about start_with exercise

---
+++ GeneralExercises/prefix.py
@@ -0,0 +1,7 @@
+
+def starts_with(string, prefix):
+    return string.startswith(prefix)
+
+print(starts_with("launch", "la"))   # True
+print(starts_with("school", "sah"))  # False
+print(starts_with("school", "sch"))  # True

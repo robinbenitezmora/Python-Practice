4e00c084492b33aada6737c6f19cd34010cdd573
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-24 10:45:25 -0500

    Use the join method

---
+++ GeneralExercises/passcode.py
@@ -0,0 +1,9 @@
+
+passcode = ['11', 'jZ5', 'hQ3f*', '8!7g3', 'p3Fs']
+
+passcode_join = '-'.join(passcode)
+
+print(passcode_join)
+
+# Write some code here.
+# Expected return value: '11-jZ5-hQ3f*-8!7g3-p3Fs'

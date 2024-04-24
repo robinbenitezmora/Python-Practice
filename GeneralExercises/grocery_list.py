7215a77b618e855a03d6ce8aa5f27600b9a6d349
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-24 11:24:43 -0500

    Refactor grocery_list.py file

--- GeneralExercises/grocery_list.py
+++ GeneralExercises/grocery_list.py
@@ -3,8 +3,14 @@ grocery_list = ['paprika', 'tofu', 'garlic', 'quinoa',
                 'carrots', 'broccoli', 'hummus']
 
 def print_grocery_list(grocery_list):
-    for i in range(len(grocery_list)):
-        list = grocery_list.pop(0)
-        print(list)
+    for item in grocery_list:
+        print (item)
+        grocery_list.index(item)
+    return grocery_list
+
+
+print(print_grocery_list(grocery_list))
+
+
 
 print_grocery_list(grocery_list)

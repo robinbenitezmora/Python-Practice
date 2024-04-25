7b719e6f138ea810e330ce2a52413acf98d75ad2
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-25 09:28:42 -0500

    Solve the bug in the confucius_says file

---
+++ Debugging/confucius_says.py
@@ -0,0 +1,11 @@
+
+def get_quote(person):
+    if person == 'Yoda':
+        return 'Do. Or do not. There is no try.'
+    if person == 'Confucius':
+        return 'I hear and I forget. I see and I remember. I do and I understand.'
+    if person == 'Einstein':
+        return 'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.'
+
+print('Confucius says:')
+print('"' + get_quote('Confucius') + '"')

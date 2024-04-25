58a7a024c28dc0ae8528feef44b96612f40f3289
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-25 07:25:36 -0500

    Create a nested dictionary

---
+++ GeneralExercises/cars.py
@@ -0,0 +1,15 @@
+
+cars = {
+    'car': {
+     'type': 'Sedan',
+      'color': 'Blue',
+      'year': 2003,
+      },
+    'truck': {
+     'type': 'Pickup',
+      'color': 'Red',
+      'year': 1998,
+     },
+}
+
+print(cars)

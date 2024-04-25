7705557994ef8261f34452e0e3b6d6b8666bd204
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-25 11:09:30 -0500

    Solve the bug in the digit_product file

---
+++ Debugging/digit_product.py
@@ -0,0 +1,12 @@
+
+def digit_product(str_num):
+    digits = [int(n) for n in str_num]
+    product = 1
+
+    for digit in digits:
+        product *= digit
+
+    return product
+
+result = digit_product('12345')
+print(result)

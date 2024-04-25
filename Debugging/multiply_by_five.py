d85ebe5bb2e4d8e5696df408506cae7c117878f9
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-25 08:35:04 -0500

    Solve the bug in the multiply_by_five file

---
+++ Debugging/multiply_by_five.py
@@ -0,0 +1,8 @@
+
+def multiply_by_five(n):
+    return n * 5
+
+print("Hello! Which number would you like to multiply by 5?")
+number = int(input())
+
+print(f"The result is {multiply_by_five(number)}!")

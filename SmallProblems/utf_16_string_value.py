f6de1af5ac9e0c8cfd1f12e477c537b6856c56c6
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-29 20:47:05 -0500

    Create utf_16_string_value.py file

---
+++ SmallProblems/utf_16_string_value.py
@@ -0,0 +1,16 @@
+
+def utf16_value(string):
+    return sum(ord(char) for char in string)
+
+# These examples should all print True
+print(utf16_value('Four score') == 984)
+print(utf16_value('Launch School') == 1251)
+print(utf16_value('a') == 97)
+print(utf16_value('') == 0)
+
+# The next three lines demonstrate that the code
+# works with non-ASCII characters from the UTF-16
+# character set.
+OMEGA = "\u03A9"              # UTF-16 character 'Ω' (omega)
+print(utf16_value(OMEGA) == 937)
+print(utf16_value(OMEGA + OMEGA + OMEGA) == 2811)    

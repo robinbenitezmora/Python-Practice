8b7bda19c4c6dd551b2c2b782bfdd443e39764e1
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-29 20:14:41 -0500

    Create leay_year.py file in order to calculate leap years in the Gregorian calendar

---
+++ SmallProblems/leap_year.py
@@ -0,0 +1,29 @@
+
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    elif (year % 100 == 0) and (year % 400 != 0):
+        return False
+    elif (year % 4 == 0) and (year % 100 != 0):
+        return True
+    else:
+        return False
+
+# These examples should all print True
+print(is_leap_year(1) == False)
+print(is_leap_year(2) == False)
+print(is_leap_year(3) == False)
+print(is_leap_year(4) == True)
+print(is_leap_year(1000) == False)
+print(is_leap_year(1100) == False)
+print(is_leap_year(1200) == True)
+print(is_leap_year(1300) == False)
+print(is_leap_year(1751) == False)
+print(is_leap_year(1752) == True)
+print(is_leap_year(1753) == False)
+print(is_leap_year(1800) == False)
+print(is_leap_year(1900) == False)
+print(is_leap_year(2000) == True)
+print(is_leap_year(2023) == False)
+print(is_leap_year(2024) == True)
+print(is_leap_year(2025) == False)

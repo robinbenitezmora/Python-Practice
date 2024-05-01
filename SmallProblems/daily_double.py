00749aace466585549784272768e734dc20fcd1d
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-01 10:19:33 -0500

    Create the comment in daily_double.py file

--- SmallProblems/daily_double.py
+++ SmallProblems/daily_double.py
@@ -1,5 +1,4 @@
-
-
+# Write a function that takes a string argument and returns a new string that contains the value of the original string with all consecutive duplicate characters collapsed into a single character.
 
 def crunch(string):
     result = ''

5b2568275d28f478a9a26065eeed532f2058fce4
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-30 23:14:02 -0500

    Add a comment in get_middle_character.py file

--- SmallProblems/get_middle_character.py
+++ SmallProblems/get_middle_character.py
@@ -1,5 +1,5 @@
 
-
+# Write a function that takes a non-empty string argument and returns the middle character(s) of the string. If the string has an odd length, you should return exactly one character. If the string has an even length, you should return exactly two characters.
 
 def center_of(string):
     if len(string) % 2 == 0:

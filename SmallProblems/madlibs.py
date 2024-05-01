6b305ff177614141e567cdb70bdd800a4ea1f909
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-01 10:49:15 -0500

    Create the madlibs.py file

---
+++ SmallProblems/madlibs.py
@@ -0,0 +1,14 @@
+
+
+
+noun = input('Enter a noun: ')
+verb = input('Enter a verb: ')
+adjective = input('Enter an adjective: ')
+adverb = input('Enter an adverb: ')
+
+def madlibs(noun, verb, adjective, adverb):
+    print(f'Do you {verb} your {adjective} {noun} {adverb}? That\'s hilarious!')
+    print(f'The {adjective} {noun} {verb}s {adverb} over the lazy dog.')
+    print(f'The {noun} {adverb} {verb}s up {adjective} Joe\'s turtle.')
+
+madlibs(noun, verb, adjective, adverb)

49d5a59274eba801721d307f6f790137d57bcce2
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-30 22:46:54 -0500

    Add a comment in odd_list.py file

--- SmallProblems/odd_lists.py
+++ SmallProblems/odd_lists.py
@@ -1,4 +1,5 @@
 
+# Write a function that returns a list that contains every other element of a list that is passed in as an argument. The values in the returned list should be those values that are in the 1st, 3rd, 5th, and so on elements of the argument list.
 
 
 def oddities(lst):

1ffadd597e34d8377a5313549a34947611a83652
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-30 22:41:02 -0500

    Create odd_list.py file

---
+++ SmallProblems/odd_lists.py
@@ -0,0 +1,14 @@
+
+
+
+def oddities(lst):
+    list = []
+    for i in range(0, len(lst), 2):
+        list.append(lst[i])
+    return list
+
+print(oddities([2, 3, 4, 5, 6]) == [2, 4, 6])  # True
+print(oddities([1, 2, 3, 4]) == [1, 3])        # True
+print(oddities(["abc", "def"]) == ['abc'])     # True
+print(oddities([123]) == [123])                # True
+print(oddities([]) == [])                      # True

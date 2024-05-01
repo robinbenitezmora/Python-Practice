cfa206ae34237fb44b7d04c9ad4018afe69aee5f
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-30 23:18:34 -0500

    Add comments to always_negative.py file

--- SmallProblems/always_negative.py
+++ SmallProblems/always_negative.py
@@ -1,5 +1,5 @@
 
-
+# Write a function that takes a number as an argument. If the argument is a positive number, return the negative of that number. If the argument is a negative number, return it as-is.
 
 def negative(num):
     if num > 0:

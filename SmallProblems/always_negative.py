c0f6ada0aa6308024d4e445bc76bb333d69c13a7
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-30 23:17:22 -0500

    Create always_negative.py file

---
+++ SmallProblems/always_negative.py
@@ -0,0 +1,12 @@
+
+
+
+def negative(num):
+    if num > 0:
+        return -num
+    else:
+        return num
+
+print(negative(5) == -5)      # True
+print(negative(-3) == -3)     # True
+print(negative(0) == 0)       # True

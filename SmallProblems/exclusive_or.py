d4ffc0c6b49d4cc695cf355647443aabfc6e14a0
robinbenitezmora <robinbenitez1990@gmail.com>
2024-04-30 22:31:59 -0500

    Add code line to exclusive_or.py file

--- SmallProblems/exclusive_or.py
+++ SmallProblems/exclusive_or.py
@@ -1,5 +1,7 @@
 
+'''The or operator returns a truthy value if either or both of its operands are truthy, a falsy value if both operands are falsy. The and operator returns a truthy value if both of its operands are truthy, and a falsy value if either operand is falsy. This works great until you need only one of two conditions to be truthy, the so-called exclusive or, also known as xor (pronounced "ECKS-or").
 
+In this exercise, you will write an xor function that takes two arguments, and returns True if exactly one of its arguments is truthy, False otherwise.'''
 
 def xor(comment1, comment2):
  if (comment1 and not comment2) or (comment2 and not comment1):

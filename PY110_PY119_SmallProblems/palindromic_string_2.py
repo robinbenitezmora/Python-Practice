be22949a904f176bc9321a4e907df0a0576d3580
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-02 18:03:14 -0500

    Create palindromic_string_2.py in Python

---
+++ PY110_PY119_SmallProblems/palindromic_string_2.py
@@ -0,0 +1,18 @@
+'''Write another function that returns True if the string passed as an argument is a palindrome, or False otherwise. This time, however, your function should be case-insensitive, and should ignore all non-alphanumeric characters. If you wish, you may simplify things by calling the is_palindrome function you wrote in the previous exercise.'''
+
+def is_real_palindrome(string):
+    string = string.lower()
+    string = ''.join([char for char in string if char.isalnum()])
+    return string == string[::-1]
+
+print(is_real_palindrome('madam') == True)           # True
+print(is_real_palindrome('356653') == True)          # True
+print(is_real_palindrome('356635') == False)         # True
+print(is_real_palindrome('356a653') == True)         # True
+print(is_real_palindrome('123ab321') == False)       # True
+
+# case doesn't matter
+print(is_real_palindrome('Madam') == True)           # True
+
+# only alphanumerics matter
+print(is_real_palindrome("Madam, I'm Adam") == True) # True

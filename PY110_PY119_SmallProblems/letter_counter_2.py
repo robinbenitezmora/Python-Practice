5098b5175730f5c9f325b868ca291dc74edaa8c0
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-02 19:20:13 -0500

    Add a comment in letter_counter_2.py file

--- PY110_PY119_SmallProblems/letter_counter_2.py
+++ PY110_PY119_SmallProblems/letter_counter_2.py
@@ -1,4 +1,4 @@
-
+# Modify the word_sizes function from the previous exercise to exclude non-letters when determining word size. For instance, the word size of "it's" is 3, not 4.
 
 def word_sizes(string):
     word_dict = {}

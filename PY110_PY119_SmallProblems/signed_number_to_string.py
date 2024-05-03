920b4362f7719d243a342612c73c6c007de685ab
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-02 21:02:28 -0500

    Add a comment in signed_number_to_string.py file

--- PY110_PY119_SmallProblems/signed_number_to_string.py
+++ PY110_PY119_SmallProblems/signed_number_to_string.py
@@ -1,4 +1,8 @@
+'''In the previous exercise, you developed a function that converts non-negative numbers to strings. In this exercise, you're going to extend that function by adding the ability to represent negative numbers as well.
 
+Write a function that takes an integer and converts it to a string representation.
+
+You may not use any of the standard conversion functions available in Python, such as str. You may, however, use integer_to_string from the previous exercise.'''
 
 import number_to_string
 

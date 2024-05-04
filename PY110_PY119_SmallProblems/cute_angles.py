df8f2416a1446b6af37c6bee6f273e6a5a41f9b0
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-03 21:12:50 -0500

    Create cute_angles.py file

---
+++ PY110_PY119_SmallProblems/cute_angles.py
@@ -0,0 +1,15 @@
+'''Write a function that takes a floating point number representing an angle between 0 and 360 degrees and returns a string representing that angle in degrees, minutes, and seconds. You should use a degree symbol (˚) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. There are 60 minutes in a degree, and 60 seconds in a minute.'''
+
+def dms(angle):
+    degrees = int(angle)
+    minutes = int((angle - degrees) * 60)
+    seconds = int(((angle - degrees) * 60 - minutes) * 60)
+    return f"{degrees}°{minutes:02d}'{seconds:02d}\""
+
+# All of these examples should print True
+print(dms(30) == "30°00'00\"")
+print(dms(76.73) == "76°43'48\"")
+print(dms(254.6) == "254°35'59\"")
+print(dms(93.034773) == "93°02'05\"")
+print(dms(0) == "0°00'00\"")
+print(dms(360) == "360°00'00\"" or dms(360) == "0°00'00\"")    

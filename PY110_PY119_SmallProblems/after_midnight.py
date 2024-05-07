c0d1b69d81d47ab7f6c55bd813cc84f409b3deb9
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-06 21:26:20 -0500

    Create after_midnight.py in order to practice Python

---
+++ PY110_PY119_SmallProblems/after_midnight.py
@@ -0,0 +1,19 @@
+'''The time of day can be represented as the number of minutes before or after midnight. If the number of minutes is positive, the time is after midnight. If the number of minutes is negative, the time is before midnight.
+
+Write a function that takes a time using this minute-based format and returns the time of day in 24-hour format (hh:mm). Your function should work with any integer input.
+
+You may not use Python's datetime module.'''
+
+def time_of_day(minutes):
+    minutes = minutes % 1440
+    hours = minutes // 60
+    minutes = minutes % 60
+    return '{:02d}:{:02d}'.format(hours, minutes)
+
+print(time_of_day(0) == "00:00")        # True
+print(time_of_day(-3) == "23:57")       # True
+print(time_of_day(35) == "00:35")       # True
+print(time_of_day(-1437) == "00:03")    # True
+print(time_of_day(3000) == "02:00")     # True
+print(time_of_day(800) == "13:20")      # True
+print(time_of_day(-4231) == "01:29")    # True    

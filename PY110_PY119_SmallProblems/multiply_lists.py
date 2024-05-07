3a419fedb03da16b15b1b05c1142076f1880641d
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-06 19:58:13 -0500

    Create multiply_lists.py in order to practice Python

---
+++ PY110_PY119_SmallProblems/multiply_lists.py
@@ -0,0 +1,8 @@
+# Write a function that takes two list arguments, each containing a list of numbers, and returns a new list that contains the product of each pair of numbers from the arguments that have the same index. You may assume that the arguments contain the same number of elements.
+
+def multiply_list(list1, list2):
+    return [list1[i] * list2[i] for i in range(len(list1))]
+
+list1 = [3, 5, 7]
+list2 = [9, 10, 11]
+print(multiply_list(list1, list2) == [27, 50, 77])  # True    

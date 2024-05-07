b67d9a443b5911c529bad44b56c14795ef70af78
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-06 21:30:56 -0500

    Create after_midnight_2.py in order to practice Python

---
+++ PY110_PY119_SmallProblems/after_midnight_2.py
@@ -0,0 +1,19 @@
+'''As seen in the previous exercise, the time of day can be represented as the number of minutes before or after midnight. If the number of minutes is positive, the time is after midnight. If the number of minutes is negative, the time is before midnight.
+
+Write two functions that each take a time of day in 24 hour format, and return the number of minutes before and after midnight, respectively. Both functions should return a value in the range 0 through 1439.
+
+You may not use Python's datetime module.'''
+
+def after_midnight(time):
+    hours, minutes = time.split(':')
+    return (int(hours) * 60 + int(minutes)) % 1440
+
+def before_midnight(time):
+    return (1440 - after_midnight(time)) % 1440
+
+print(after_midnight("00:00") == 0)     # True
+print(before_midnight("00:00") == 0)    # True
+print(after_midnight("12:34") == 754)   # True
+print(before_midnight("12:34") == 686)  # True
+print(after_midnight("24:00") == 0)     # True
+print(before_midnight("24:00") == 0)    # True    

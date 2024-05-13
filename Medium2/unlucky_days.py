b6834f67e98a6f5169dfa1b0ba9da6abd054f6f9
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-13 15:01:02 -0500

    Add a comment to unlucky_days.py Python exercise

--- Medium2/unlucky_days.py
+++ Medium2/unlucky_days.py
@@ -1,4 +1,4 @@
-
+'''Some people believe that Fridays that fall on the 13th day of the month are unlucky days. Write a function that takes a year as an argument and returns the number of Friday the 13ths in that year. You may assume that the year is greater than 1752, which is when the United Kingdom adopted the modern Gregorian Calendar. You may also assume that the same calendar will remain in use for the foreseeable future.'''
 
 def friday_the_13ths(year):
     import datetime

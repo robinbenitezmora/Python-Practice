20a5965c5b6a129b5174dec4612e94e93ecf37f9
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-16 10:31:47 -0500

    Add personal identification fields in the Principal window

--- Vacations/principal.py
+++ Vacations/principal.py
@@ -24,14 +24,48 @@ class Principal:
         self.background = PhotoImage(file = r'C:\Users\Robin\PYTHON PROJECTS\Launch_School\Python-Practice\Vacations\coca-cola-p.png')
         Label(self.window, image=self.background, bg='red').place(x=0, y=0)
 
-        # Labels
+        # Label Welcome
         self.l_welcome = Label(self.window, text='Welcome!')
         self.l_welcome.config(font=('Andale Mono Regular', 20, 'bold'), bg='red', fg='white')
         self.l_welcome.place(x=380, y=35)
 
+        # Label Detail
         self.l_detail = Label(self.window, text='Data of the worker for the Vacations Calcule')
         self.l_detail.config(font=('Andale Mono Regular', 18, 'bold'), bg='red', fg='white')
         self.l_detail.place(x=60, y=110)
+
+        # Label Name
+        self.l_name = Label(self.window, text='Name:')
+        self.l_name.config(font=('Andale Mono Regular', 12, 'bold'), bg='red', fg='white')
+        self.l_name.place(x=60, y=160)
+
+        # Entry Name
+        self.data_name = StringVar()
+        self.e_name = Entry(self.window, textvariable=self.data_name, bd=2, bg='#eee8e8', fg='red')
+        self.e_name.config(font=('Andale Mono Regular', 12, 'bold'), width=27)
+        self.e_name.place(x=60, y=190)
+
+        # Label First Name
+        self.l_first_name = Label(self.window, text='First Name:')
+        self.l_first_name.config(font=('Andale Mono Regular', 12, 'bold'), bg='red', fg='white')
+        self.l_first_name.place(x=60, y=240)
+
+        # Entry First Name
+        self.data_first_name = StringVar()
+        self.e_first_name = Entry(self.window, textvariable=self.data_first_name, bd=2, bg='#eee8e8', fg='red')
+        self.e_first_name.config(font=('Andale Mono Regular', 12, 'bold'), width=27)
+        self.e_first_name.place(x=60, y=270)
+
+        # Label Last Name
+        self.l_last_name = Label(self.window, text='Last Name:')
+        self.l_last_name.config(font=('Andale Mono Regular', 12, 'bold'), bg='red', fg='white')
+        self.l_last_name.place(x=60, y=320)
+
+        # Entry Last Name
+        self.data_last_name = StringVar()
+        self.e_last_name = Entry(self.window, textvariable=self.data_last_name, bd=2, bg='#eee8e8', fg='red')
+        self.e_last_name.config(font=('Andale Mono Regular', 12, 'bold'), width=27)
+        self.e_last_name.place(x=60, y=350)
 
         self.window.mainloop()
 

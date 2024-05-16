3720bdcc2315312473fef126771c471e78e97685
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-15 21:03:12 -0500

    Create the License window

---
+++ Vacations/license.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+class License:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('TERMS AND CONDITIONS')
+        self.window.resizable(False, False)
+        self.window.geometry('600x360+400+200')
+
+        self.window.mainloop()
+
+License()
+
+

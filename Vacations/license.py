344f9abe0ab5d1e8c1a0d884fde71698ef741436
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-15 21:08:21 -0500

    Add the representative icon in corner of the License window

--- Vacations/license.py
+++ Vacations/license.py
@@ -6,6 +6,8 @@ class License:
         self.window.title('TERMS AND CONDITIONS')
         self.window.resizable(False, False)
         self.window.geometry('600x360+400+200')
+        self.window.configure(bg='white')
+        self.window.iconbitmap(r'C:\Users\Robin\PYTHON PROJECTS\Launch_School\Python-Practice\Vacations\icon0.ico')
 
         self.window.mainloop()
 

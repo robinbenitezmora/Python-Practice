81896910fb7ded3b72b6b81d0e8680aa789bf588
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-16 10:07:28 -0500

    Add menues in the Principal window

--- Vacations/principal.py
+++ Vacations/principal.py
@@ -9,6 +9,17 @@ class Principal:
         self.window.configure(bg='red')
         self.window.iconbitmap(r'C:\Users\Robin\PYTHON PROJECTS\Launch_School\Python-Practice\Vacations\icon0.ico')
 
+        # Options Menu
+        self.menubar1 = Menu(self.window)
+        self.window.config(menu=self.menubar1)
+        self.options1 = Menu(self.menubar1, tearoff=0)
+        self.options2 = Menu(self.menubar1, tearoff=0)
+        self.menubar1.add_cascade(label='Options', menu=self.options1)
+        self.options1.add_command(label='Exit', font=('Arial', 10, 'bold'))
+
+        self.menubar1.add_cascade(label='About of..', menu=self.options2)
+        self.options2.add_command(label='Info', font=('Arial', 10, 'bold'))
+
         self.window.mainloop()
 
 Principal()

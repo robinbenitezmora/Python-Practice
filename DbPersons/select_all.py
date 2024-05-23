c2e89860e9a7931ddbe4b23002f9ce85a2ef6a7a
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-23 08:35:16 -0500

    Create select_all.py file

---
+++ DbPersons/select_all.py
@@ -0,0 +1,34 @@
+# Import psycopg2 module
+import psycopg2
+
+# Create connection
+connection=psycopg2.connect(user='postgres',
+                            password='peti2020#',
+                            host='127.0.0.1',
+                            port='5432',
+                            database='db_persons'
+                            )
+
+# Use cursor
+cursor=connection.cursor()
+
+# Create sql sentence
+sql='SELECT * FROM persons'
+
+# Execute method
+cursor.execute(sql)
+
+# Fetch all records
+records=cursor.fetchall()
+
+# Show records
+for record in records:
+    print(record)
+
+# Close cursor and connection
+cursor.close()
+connection.close()
+
+# End
+print('End of the program')
+# In this case, the SELECT sentence is used to retrieve all records from the table. The fetchall method is used to retrieve all records. The records are displayed using a for loop. Finally, the cursor and connection are closed.

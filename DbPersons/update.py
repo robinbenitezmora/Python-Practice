eef10e83dd31de625ca0cbc9a257b815d614b087
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-23 07:59:51 -0500

    Refactor insert.py file

--- DbPersons/update.py
+++ DbPersons/update.py
@@ -1,7 +1,7 @@
 # Import the psycopg2 module
-
 import psycopg2
 
+# Create connection
 connection=psycopg2.connect(user='postgres',
                             password='peti2020#',
                             host='127.0.0.1',
@@ -9,10 +9,11 @@ connection=psycopg2.connect(user='postgres',
                             database='db_persons'
                             )
 
+# Use cursor
 cursor=connection.cursor()
 
-# sql sentence
-sql='UPDATE FROM persons WHERE id_person=%s, name=%s, last_name=%s, age=%s WHERE id_person=%s'
+# Create sql sentence
+sql='UPDATE FROM persons SET name=%s, last_name=%s, age=%s WHERE id_person=%s'
 
 # Request user data
 id_person=input('Enter the id of the person you want to edit: ')
@@ -20,27 +21,27 @@ name=input('Enter your name: ')
 last_name=input('Enter your last name: ')
 age=input('Enter your age: ')
 
-# Collect data
-data(name, last_name, age, id_person)
+# Collecting data
+data=(name, last_name, age, id_person)
 
-# Execute method
-cursor.execute(sql,(id_person,))
+# Use Execute method
+cursor.execute(sql, data)
 
 # Save changes
 connection.commit()
+
+# Count the amount of changes
+count=cursor.rowcount
+
+# Show user message
+print(f'Record updated: {count}')
 
 # Close cursor and connection
 cursor.close()
 connection.close()
 
-# User message
-print('Person record upgraded successfully')
-
-# End of the program
+# End
 print('End of the program')
 
-
-
-
-
-
+# In this case, the UPDATE sentence is used to modify a record in the table. The user is asked to enter the id of the person to be edited, as well as the new data. The data is passed as a parameter to the execute method. The number of records modified is stored in the count variable. Finally, the cursor and connection are closed.
+#

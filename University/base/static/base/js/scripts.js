1f9fae7d2fa7c53274a66dcf90b609ae01214ada
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-27 10:34:47 -0500

    Create js.css file

---
+++ University/base/static/base/js/scripts.js
@@ -0,0 +1,8 @@
+// Masks
+$(document).ready(function() {
+    $('#student_cpf').mask('000.000.000-00');
+});
+
+$(document).ready(function() {
+    $('#new_student_certificate').mask('000000 00 00 0000 0 00000 000 0000000 00');
+});

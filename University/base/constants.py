2c3f127d0c50803fc46fd794263a6e1568f07b76
robinbenitezmora <robinbenitez1990@gmail.com>
2024-05-26 20:34:13 -0500

    Create constants.py file

---
+++ University/base/constants.py
@@ -0,0 +1,10 @@
+'''
+Module with Constants for use in defaults attributes in models
+'''
+
+# Constants for use in defaults attributes in models
+DDD = 94
+STATE = 'PA'
+CITY = 'Manizales'
+CEP = '170004'
+COUNTRY = 'Colombia'

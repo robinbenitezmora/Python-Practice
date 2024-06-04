f443a062972763293401962fca80651f829da29e
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-04 18:18:19 -0500

    Create models.py file

---
+++ University/classes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Class(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name

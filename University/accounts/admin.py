025244291f0aa1622e4f8277f0aa0e291ac2caba
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-05 08:44:46 -0500

    Create admin.py file with code lines in accounts folder

---
+++ University/accounts/admin.py
@@ -0,0 +1,21 @@
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'first_name', 'last_name', 'department', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'department')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'first_name', 'last_name', 'department', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ['username', 'first_name', 'last_name', 'department']
+    ordering = ['first_name']
+    filter_horizontal = ['groups', 'user_permissions']

610b073b489a1edd72f57c06e0fc3d8c822f842c
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-05 09:49:39 -0500

    Add importations to admin.py file with code lines in accounts folder

--- University/accounts/admin.py
+++ University/accounts/admin.py
@@ -1,3 +1,9 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.forms import CustomUserCreateForm, CustomUserChangeForm
+from accounts.models import CustomUser
+
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm

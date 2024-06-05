7e3f0137c6e113dcd6c79ae201d87c5945506bc3
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-05 08:51:05 -0500

    Update forms.py file with code lines in accounts folder

--- University/accounts/forms.py
+++ University/accounts/forms.py
@@ -7,13 +7,29 @@ class CustomUserCreateForm(UserCreationForm):
         class Meta():
             model = CustomUser
             fields = ('first_name', 'last_name', 'username', 'department')
-            labels = {'username': 'Username/Email'}
+            labels = {'username': 'User'}
 
         def save(self, commit=True):
             user = super().save(commit=False)
             user.set_password(self.cleaned_data['password1'])
-            user.email = self.cleaned_data['username']
+            user.username = self.cleaned_data['username']
 
             if commit:
                 user.save()
             return user
+
+class CustomUserChangeForm(UserChangeForm):
+        
+            class Meta():
+                model = CustomUser
+                fields = ('first_name', 'last_name', 'username', 'department')
+                labels = {'username': 'User'}
+
+            def save(self, commit=True):
+                user = super().save(commit=False)
+                user.set_password(self.cleaned_data['password1'])
+                user.username = self.cleaned_data['username']
+
+                if commit:
+                    user.save()
+                return user

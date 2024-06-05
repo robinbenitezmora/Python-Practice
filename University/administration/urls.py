bbe2c1f30f17ba02f95e4f740d877ef32a32daec
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-05 10:02:58 -0500

    Update urls.py file with code lines in administration folder

--- University/administration/urls.py
+++ University/administration/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from administration.views import (
-    IndexAdminstrationView
+    IndexAdminstrationView,
+    AdministrationSearchView,
 )
 
 urlpatterns = [
     path('', IndexAdminstrationView.as_view(), name='index-administration'),
+    path('search/', AdministrationSearchView.as_view(), name='search-administration'),
 ]

8819f1d37fdd91204806267b808bb7fdee414d72
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-05 09:16:59 -0500

    Update urls.py file with code lines in accounts folder

--- University/accounts/urls.py
+++ University/accounts/urls.py
@@ -1,27 +1,20 @@
-from django.contrib import admin
 from django.urls import path, include
 from accounts.views import (
-    UserCreate,
-    UserChange,
-    UserDelete,
-    PasswordChange,
-    PasswordReset,
-    PasswordResetConfirm,
-    PasswordResetComplete
-)
-
-from django.contrib.auth.views import (
-    PasswordResetDoneView,
+    UserLogin,
+				UserCreate,
+				UserChange,
+				UserDelete,
+				UserListView,
 )
 
 urlpatterns = [
-	path('', include('django.contrib.auth.urls')),
-	path('user-new/', UserCreate.as_view(), name='user-new'),
-	path('<int:pk>/update/', UserChange.as_view(), name='user-change'),
-	path('password-change/', PasswordChange.as_view(), name='password-change'),
-	path('password-reset/', PasswordReset.as_view(), name='password-reset'),
-	path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-	path('reset/<uidb64>/<token>/', PasswordResetConfirm.as_view(), name="password_reset_confirm"),
-	path('login/', PasswordResetComplete.as_view(), name='password_reset_complete'),
-	path('<int:pk>/delete/', UserDelete.as_view(), name='user-delete'),
+				path('login/', UserLogin.as_view(), name='login'),
+				path('users/', UserListView.as_view(), name='users'),
+				path('user-new/', UserCreate.as_view(), name='user-new'),
+				path('<int:pk>/update', UserChange.as_view(), name='user-change'),
+				path('<int:pk>/delete', UserDelete.as_view(), name='user-delete'),
+				path('', include('django.contrib.auth.urls')),
 ]
+				
+
+

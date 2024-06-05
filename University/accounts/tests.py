12c30ce5d93a33f9a1326bf0a047a9bc07c26448
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-05 09:07:25 -0500

    Update tests.py file with code lines in accounts folder

--- University/accounts/tests.py
+++ University/accounts/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
-# Create your tests here
+# Create your tests here.
+class TestCustomUser(TestCase):
+    def test_create_user(self):
+        pass
+
+    def test_create_superuser(self):
+        pass
+
+    def test_user_str(self):
+        pass
+
+    def test_user_verbose_name(self):
+        pass
+
+    def test_user_verbose_name_plural(self):
+        pass
+
+    def test_user_ordering(self):
+        pass
+
+    def test_user_department_choices(self):
+        pass
+
+    def test_user_is_staff(self):
+        pass
+
+    def test_user_department(self):
+        pass
+
+    def test_user_objects(self):
+        pass
+
+    def test_user_meta(self):
+        pass
+
+    def test_user_str(self):
+        pass

3433250ade6c90623b0fd489063f755bcd99f426
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-10 19:59:42 -0500

    Refactor urls.py file in students folder

--- University/students/urls.py
+++ University/students/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from students.views import (
     StudentIndexView,
-    StudentNewView
+    StudentNewView,
+    StudentListView,
+    StudentEfectiveView,
+    StudentUpdateView,
+    StudentDeleteView,
 )
 
 urlpatterns = [
     path('', StudentIndexView.as_view(), name='index_student'),
-    path('new_student/', StudentNewView.as_view(), name='new_student')
+    path('students/', StudentListView.as_view(), name='students'),
+    path('students/new/', StudentNewView.as_view(), name='new_student'),
+    path('students/efective/', StudentEfectiveView.as_view(), name='efective_student'),
+    path('<int:pk>change/', StudentUpdateView.as_view(), name='student_change'),
+    path('<int:pk>delete/', StudentDeleteView.as_view(), name='student_delete'),
 ]

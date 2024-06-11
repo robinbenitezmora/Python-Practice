07eb895b4bda9e2a280aa5507cbaa0d2e4fa22ca
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-11 08:56:22 -0500

    Refactor 0001_initial.py file

--- University/base/migrations/0001_initial.py
+++ University/base/migrations/0001_initial.py
@@ -9,27 +9,70 @@ class Migration(migrations.Migration):
     
         operations = [
             migrations.CreateModel(
+                name='AreaKnowledge',
+                fields=[
+                    ('area_code', models.CharField(max_length=2, primary_key=True, serialize=False,unique=True, verbose_name='Curricular component code')),
+                    ('curricular_component', models.CharField(max_length=50, unique=True, verbose_name='Curricular Componet')),
+                    ('knowledge_area', models.CharField(max_length=50, verbose_name='Knowledge Area')),
+                ],
+                options={
+                    'verbose_name': 'Curricular Component',
+                    'verbose_name_plural': 'Curricular Components',
+                    'ordering': ['curricular_component']
+                },
+            ),
+            migrations.CreateModel(
+                name='ComplementActivity',
+                fields=[
+                    ('area_code', models.CharField(max_length=2, verbose_name='Area Code')),
+                    ('area_name', models.CharField(max_length=50, verbose_name='Area Name')),
+                    ('subarea_code', models.CharField(max_length=4, verbose_name='Subarea Code')),
+                    ('subarea_name', models.CharField(max_length=50, verbose_name='Subarea Name')),
+                    ('activity_code', models.CharField(max_length=5, primary_key=True, serialize= False, , unique=True, verbose_name='Activity Code')),
+                    ('activity_name', models.CharField(max_length=100, verbose_name='Activity name')),
+                ],
+                options={
+                    'verbose_name': 'Complementary activity',
+                    'verbose_name_plural': 'Complementary activities',
+                    'ordering': ['area_code', 'subarea-code', 'activity_name']
+                },
+            ),
+            migrations.CreateModel(
+                name='CourseFS',
+                fields=[
+                    ('cfs_code', models.CharField(max_length=6, primary_key=True, serialize=False, unique=True, verbose_name='Higher Education Course Code')),
+                    ('cfs_name_grade', models.CharField(max_length=100, unique=True, verbose_name='Name of higher education course')),
+                ],
+                options={
+                    'verbose_name': 'Higher Education Course',
+                    'verbose_name_plural': 'Higher Education Courses',
+                    'ordering': ['cfs_name_grade']
+                },
+            ),
+            migrations.CreateModel(
                 name='City',
-            fields=[
-                ('city_code', models.CharField(max_length=7, primary_key=True, serialize=False, unique=True, verbose_name='Code of city')),
-                ('city_uf', models.CharField(max_length=2, verbose_name='UF')),
-                ('city_name', models.CharField(max_length=50, verbose_name='City')),
-            ],
-            options={
-                'verbose_name': 'City',
-                'verbose_name_plural': 'Cities',
-            },
-        ),
-        migrations.CreateModel(
-            name='Country',
-            fields=[
-                ('code_country', models.CharField(max_length=3, primary_key=True, serialize=False, unique=True, verbose_name='Code of Country')),
-                ('country_name', models.CharField(max_length=50, verbose_name='Country')),
-            ],
-            options={
-                'verbose_name': 'Country',
-                'verbose_name_plural': 'Countries',
-            },
-        ),
-    ]
-    
+                fields=[
+                    ('city_code', models.CharField(max_length=7, primary_key=True, serialize=False, unique=True, verbose_name='City Code')),
+                    ('city_name', models.CharField(max_length=50, verbose_name='City Name')),
+                    ('city_uf', models.CharField(max_length=2, verbose_name='UF')),
+                ],
+                options={
+                    'verbose_name': 'City',
+                    'verbose_name_plural': 'Cities',
+                    'ordering': ['city_name']
+                },
+            ),
+            migrations.CreateModel(
+                name='Country',
+                fields=[
+                    ('country_code', models.CharField(max_length=3, primary_key=True, serialize=False, unique=True, verbose_name='Country Code')),
+                    ('country_name', models.CharField(max_length=50, verbose_name='Country Name')),
+                ],
+                options={
+                    'verbose_name': 'Country',
+                    'verbose_name_plural': 'Countries',
+                    'ordering': ['country_name']
+                },
+            ),
+        ]
+        

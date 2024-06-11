d14496113e4d424cc337dfc8514bf02866bd533c
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-11 10:29:04 -0500

    Refactor constants.py file in base folder

--- University/base/constants.py
+++ University/base/constants.py
@@ -1,10 +1,20 @@
 '''
-Module with Constants for use in defaults attributes in models
+    Module with Constants for use in defaults attributes in models
 '''
 
-# Constants for use in defaults attributes in models
+# ---- Address ---- #
 DDD = 94
 STATE = 'PA'
 CITY = 'Manizales'
 CEP = '170004'
 COUNTRY = 'Colombia'
+
+# ---- Date ---- #
+CURRENT_YEAR = '2024'
+
+# ---- Classes ---- #
+START_TIME_MORNING = '07:30'
+START_TIME_AFTERNOON = '13:00'
+
+END_TIME_MORNING = '12:00'
+END_TIME_AFTERNOON = '17:15'

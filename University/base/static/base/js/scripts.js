dbeb2f22d3a2155ed74f251c27921b3de55d8165
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-11 09:09:42 -0500

    Refactor style.css file in base folder

--- University/base/static/base/js/scripts.js
+++ University/base/static/base/js/scripts.js
@@ -1,8 +1,35 @@
 // Masks
-$(document).ready(function() {
+$(document).ready(function(){
     $('#student_cpf').mask('000.000.000-00');
-});
-
-$(document).ready(function() {
-    $('#new_student_certificate').mask('000000 00 00 0000 0 00000 000 0000000 00');
-});
+    $('#student_affiliation1_cpf').mask('000.000.000-00');
+    $('#student_affiliation2_cpf').mask('000.000.000-00');
+    $('#teacher_cpf').mask('000.000.000-00');
+  });
+  
+  $(document).ready(function(){
+    $('#student_new certificate').mask('000000 00 00 0000 0 00000 000 0000000 00');
+  });
+  
+  $(document).ready(function(){
+    $('#student_cep_residence').mask('00000-000');
+    $('#teacher_cep_residence').mask('00000-000');
+  });
+  
+  $(document).ready(function(){
+    $('#student_birth_date').mask('00/00/0000');
+    $('#teacher_birth_date').mask('00/00/0000');
+  });
+  
+  
+  // Cokie Consent
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#102d69"
+      },
+      "button": {
+        "background": "#FBBA00"
+      }
+    },
+    "theme": "classic",
+  });

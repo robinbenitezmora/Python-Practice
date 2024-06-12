2e0dde7221eefa8711d57fbd8fd28b84643b0682
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-12 07:37:15 -0500

    Refactor forms.py file in principal folder

--- University/principal/forms.py
+++ University/principal/forms.py
@@ -1,30 +1,11 @@
 from django import forms
 
-from principal.models import (
-    LectiveYear,
-    BasicStage,
-    SchoolYear
-)
+from principal.models import Discipline
 
-class LectiveYearForm(forms.ModelForm):
+class DisciplineForm(forms.ModelForm):
     class Meta:
-        model = LectiveYear
+        model = Discipline
         fields = [
-            'lective_year_name'
-        ]
-
-class BasicStageForm(forms.ModelForm):
-    class Meta:
-        model = BasicStage
-        fields = [
-            'basic_stage_name',
-            'basic_stage_year',
-        ]
-
-class SchoolYearForm(forms.ModelForm):
-    class Meta:
-        model = SchoolYear
-        fields = [
-            'school_year_name',
-            'school_year_stage',
+            'discipline_name',
+            'discipline_teacher',
         ]

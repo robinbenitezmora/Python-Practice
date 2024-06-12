e9751f94d8005e417aaa1eac494f17e8ff7531e6
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-12 07:52:30 -0500

    Refactor urls.py file in principal folder

--- University/principal/urls.py
+++ University/principal/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-
+from principal.views import (
+    DisciplineListView,
+    DisciplineNewView,
+    DisciplineUpdateView,
+)
 
 urlpatterns = [
- 
+    path('disciplines/', DisciplineListView.as_view(), name='disciplines'),
+    path('disciplines/new/', DisciplineNewView.as_view(), name='discipline_new'),
+    path('<int:pk>/change/', DisciplineUpdateView.as_view(), name='discipline_update'),
 ]

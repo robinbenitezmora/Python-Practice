1a20e633f38c690f98c0a08062f67f427718a2a8
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-13 14:11:27 -0500

    Modify urls.py file in professor folder

--- University/teachers/urls.py
+++ University/teachers/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from teachers.views import (
-	IndexTeacherView
+				TeacherListView,
+				IndexTeacherView,
+				TeacherNewView,
+				TeacherUpdateView,
+				TeacherDeleteView,
 )
 
-
 urlpatterns = [
-	path('', IndexTeacherView.as_view(), name='index_teacher')
+				path('teachers/', TeacherListView.as_view(), name='teachers'),
+				path('', IndexTeacherView.as_view(), name='index_teacher'),
+				path('teachers_new/', TeacherNewView.as_view(), name='new_teacher'),
+				path('<int:pk>/change', TeacherUpdateView.as_view(), name='change_teacher'),
+				path('<int:pk>/delete', TeacherDeleteView.as_view(), name='delete_teacher'),
 ]

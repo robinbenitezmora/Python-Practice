622812ca4f84a9d96b3ed5040f28fca0978fcea9
robinbenitezmora <robinbenitez1990@gmail.com>
2024-06-18 22:21:29 -0500

    Update the get_links.py file as a Python miniproject

--- MiniProjects/All_links/get_links.py
+++ MiniProjects/All_links/get_links.py
@@ -1,11 +1,11 @@
-import request as rq
 from bs4 import BeautifulSoup
+import requests as rq
 
-url = imput('Enter Link: ')
+url = input('Enter Link: ')
 if ('https' or 'http') in url:
     data = rq.get(url)
 else:
-    data = rq.get('https://' + url)
+    data = rq.get('https://'+url)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 links = []
@@ -13,10 +13,10 @@ links = []
 for link in soup.find_all('a'):
     links.append(link.get('href'))
 
-# Writing the output to a file (myLinks.txt) instead of stdout
-# You can change 'a' to 'w' to overwrite the files each time
+# Writing the output to a file (myLinks.txt) instead of to stdout
+# You can change 'a' to 'w' to overwrite the file each time
+
 with open('myLinks.txt', 'a') as saved:
     for link in links:
-        saved.write(link + '\n')
+        saved.write(link+'\n')
 print('Links saved to myLinks.txt')
-

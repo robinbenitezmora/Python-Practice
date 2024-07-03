88f27020ead4178860f0efd02ed615eae1fe3dac
robinbenitezmora <robinbenitez@yahoo.com>
2024-07-03 11:48:30 -0500

    Update password_manager.py Python file

--- Beginner/password_manager.py
+++ Beginner/password_manager.py
@@ -1,32 +1,86 @@
-pwd = input('Enter the master password: ')
+from cryptography.fernet import Fernet
+
+master_pwd = input("What is the master password? ")
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
-            user, password = data.split('|')
-            print('User:', user, 'Password:', password)
+            user, passw = data.split("|")
+            print("User:", user, "Password:", passw)
 
 def add():
-    name = input('Account name: ')
-    pwd = input('Password: ')
-    
+    name = input("Account name: ")
+    pwd = input("Password: ")
+
     with open('passwords.txt', 'a') as f:
-        f.write(name + '|' + pwd + '\n')
+        f.write(name + "|" + pwd + "\n")
+
+def update():
+    name = input("Account name: ")
+    pwd = input("New password: ")
+
+    with open('passwords.txt', 'r') as f:
+        lines = f.readlines()
+
+    with open('passwords.txt', 'w') as f:
+        for line in lines:
+            user, passw = line.strip().split("|")
+            if user == name:
+                f.write(user + "|" + pwd + "\n")
+            else:
+                f.write(user + "|" + passw + "\n")
+
+def delete():
+    name = input("Account name: ")
+
+    with open('passwords.txt', 'r') as f:
+        lines = f.readlines()
+
+    with open('passwords.txt', 'w') as f:
+        for line in lines:
+            user, passw = line.strip().split("|")
+            if user != name:
+                f.write(user + "|" + passw + "\n")
+
+def encrypt():
+    with open('passwords.txt', 'r') as f:
+        data = f.read()
+
+    f = Fernet(master_pwd)
+    encrypted = f.encrypt(data.encode())
+
+    with open('passwords.txt', 'w') as f:
+        f.write(encrypted.decode())
+
+def decrypt():
+    with open('passwords.txt', 'r') as f:
+        data = f.read()
+
+    f = Fernet(master_pwd)
+    decrypted = f.decrypt(data.encode())
+
+    with open('passwords.txt', 'w') as f:
+        f.write(decrypted.decode())
 
 while True:
-    if pwd == '123':
-        mode = input('Would you like to add a new password or view existing ones (view, add), press q to quit? ').lower()
-        if mode == 'q':
-            break
-        if mode == 'view':
-            view()
-        elif mode == 'add':
-            add()
-        else:
-            print('Invalid mode.')
-            continue
-    else:
-        print('Wrong password. Try again.')
+    mode = input("Would you like to add a new password, view existing passwords, update a password, or delete a password (add, view, update, delete)? If you want to exit, type 'exit'. ").lower()
+    if mode == 'exit':
+        encrypt()
         break
-print('Goodbye!')
+    elif mode == 'view':
+        decrypt()
+        view()
+    elif mode == 'add':
+        decrypt()
+        add()
+    elif mode == 'update':
+        decrypt()
+        update()
+    elif mode == 'delete':
+        decrypt()
+        delete()
+    else:
+        print("Invalid mode.")
+        continue
+    encrypt()

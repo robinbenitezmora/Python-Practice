a9055cd2ebd5577f5f7a08def2f23816c3858557
robinbenitezmora <robinbenitez@yahoo.com>
2024-07-18 08:01:40 -0500

    Create config.py Python file

---
+++ Intermediate/reddit_comment_bot/config.py
@@ -0,0 +1,12 @@
+REDDIT_USERNAME = 'YourRedditUsername'
+REDDIT_PASSWORD = 'YourRedditPassword'
+
+REDDIT_CLIENT_ID = 'YourRedditClientId'
+REDDIT_CLIENT_SECRET = 'YourRedditClientSecret'
+
+REDDIT_USER_AGENT = 'YourUniqueUserAgent'
+TARGET_SUBREDDIT = 'test'
+TARGET_STRING = 'sample user comment'
+REPLY_MESSAGE = 'Hey, I am a bot. You mentioned the word "sample user comment" in your comment. I am here to help you.'
+
+SLEEP_DURATION = 10

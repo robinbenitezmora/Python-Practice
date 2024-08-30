be51a5ad4d1c310db45a8ed670f423db694ed54c
robinbenitezmora <robinbenitez@yahoo.com>
2024-08-29 20:07:13 -0500

    Create sudoku_puzzle.py file

---
+++ Intermediate/sudoku_puzzle.py
@@ -0,0 +1,69 @@
+import copy, random, sys
+
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print('-' * 21)
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print('|', end=' ')
+            print(board[i][j], end=' ')
+        print()
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, num, pos):
+    for i in range(9):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for i in range(1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
+
+def main():
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+    print('Sudoku puzzle:')
+    print_board(board)
+    print()
+    solve(copy.deepcopy(board))
+    print('Solved Sudoku puzzle:')
+    print_board(board)
+
+if __name__ == '__main__':
+    main()
+    
